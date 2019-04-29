904b8c720c20a0f218d0d480ccffb01ddc408e9f
Julio Pérez <juliolpj@hotmail.com>
2019-04-28 21:59:21 -0300

    deploy commit 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDbLNWzHhl1dUWFohXzkv1U7ymzgAZgy68",
+    authDomain: "fobokac-01.firebaseapp.com",
+    databaseURL: "https://fobokac-01.firebaseio.com",
+    projectId: "fobokac-01",
+    storageBucket: "fobokac-01.appspot.com",
+    messagingSenderId: "1019734820364"
+  }
 };
